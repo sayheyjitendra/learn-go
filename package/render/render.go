04ecb50804427156aab9676b30bd9978b41e5365
Jitendra <jitendraks1803@gmail.com>
2022-01-07 20:55:25 +0530

    fixed template path

--- package/render/render.go
+++ package/render/render.go
@@ -7,7 +7,7 @@ import (
 )
 
 func RenderTemplate(w http.ResponseWriter, tmpl string){
-	parsedTemplate , _ := template.ParseFiles("../templates/" + tmpl)
+	parsedTemplate , _ := template.ParseFiles("./templates/" + tmpl)
 	err := parsedTemplate.Execute(w, nil)
 
 	if err != nil {
